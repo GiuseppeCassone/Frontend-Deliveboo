5636a58268f323de6adb604ac306de5fcd076a3b
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-05-19 11:04:29 +0200

    fix bootstrap

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
+import "bootstrap/dist/css/bootstrap.css"
 import './style.scss'
 import App from './App.vue'
 
 
 import { router } from './router' 
 
-import * as bootstrap from 'bootstrap'
+import "bootstrap/dist/js/bootstrap.js"
+// import * as bootstrap from 'bootstrap'
 
 createApp(App).use(router).mount('#app')
