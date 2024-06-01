92b0ebe1fc5114f69fd8fbb1004657d7f66d444f
albertoferri <falberto99@gmail.com>
2024-06-01 17:30:40 +0200

    fix jumbotron carousel

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -35,46 +35,36 @@ export default {
 </script>
 <template>
 
-    <div class="container-fluid py-4 my-bg-img">
+    <div class="container-fluid w-100 py-4 my-bg-img">
         <div class="container bg-opacity-75 pt-4 rounded-3" style=" background-color: rgba(0, 0, 0, 0.4);">
 
-            <div class="jumbotron p-5 ">
-                <h1 class="display-4 text-white">Benvenuti su <br> <span class="display-2 fw-bolder">WOW DELIVE</span></h1>
-                <p class="lead text-white">
-                    Tu scegli il piatto,<br> al resto ci pensiamo <span class="fw-bolder ">Noi</span>.
-                </p>
-
-
-                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner ">
-                        <div v-for="(homedish, index) in homedishes" :key="homedish.id"
-                            :class="['carousel-item', { active: index === 0 }]">
-                            <div class="card mb-3" style="max-width: 540px; margin: auto;">
-                                <div class="row g-0">
-                                    <div class="col-md-4">
-                                        <img :src="homedish.img.includes('https') ? homedish.img : this.apiImageUrl"
-                                            class="my-img img-fluid rounded-start" :alt="homedish.name" />
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class="card-body p-0 ps-2">
-                                            <h5 class="card-title">{{ homedish.name }}</h5>
-                                            <p class="card-text">{{ homedish.description }}</p>
-                                            <router-link
-                                                :to="{ name: 'info-restaurant', params: { id: homedish.restaurant_id } }"
-                                                class="btn my-btn-color rounded-3 mb-5">Vai al ristorante</router-link>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+            <div class="jumbotron p-5 d-flex flex-column">
+                <div class="row d-flex justify-content-between between mb-3">
+                    <h1 class="col-12 col-lg-8 display-4 text-white">Benvenuti su <br> <span class="display-2 fw-bolder">WOW DELIVE</span></h1>
+                    <div class="col-12 col-lg-4 lead text-white d-flex align-items-center">
+                       <p class="mb-0">Tu scegli il piatto,<br> al resto ci pensiamo <strong>Noi</strong>.</p>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-                        data-bs-slide="prev">
+                </div>
+
+                <div id="carouselExampleCaptions" class="carousel slide align-self-center" >
+                    <div class="carousel-inner">
+                        <routerLink :to="{ name: 'info-restaurant', params: { id: homedish.restaurant_id } }" 
+                        v-for="(homedish, index) in homedishes" :key="homedish.id"
+                        :class="['carousel-item', { active: index === 0 }]">
+                            <div class="container d-flex justify-content-center div-img">
+                                <img :src="homedish.img.includes('https') ? homedish.img : this.apiImageUrl"
+                                class="my-img img-fluid" :alt="homedish.name" />
+                            </div>
+                            <div class="carousel-caption d-block">
+                                <h5 class="card-title text-center ">{{ homedish.name }}</h5>
+                            </div>
+                        </routerLink>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Previous</span>
                     </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-                        data-bs-slide="next">
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
@@ -90,7 +80,7 @@ export default {
 @use '../styles/variables' as *;
 
 .my-bg-img {
-    background-image: url('../public/images/jumbo-img.jpg');
+    background-image: url('../images/jumbo-img.jpg');
     background-size: cover;
 }
 
@@ -99,8 +89,46 @@ export default {
     object-fit: cover;
 }
 
-.my-btn-color{
-    background-color: $secondColor;
-    color:white;
+
+.btn-color{
+
+  background-color: $secondColor;
+  border-color: $primaryColor;
+
+  &:hover{
+  background-color: $primaryColor;
+  }
+
 }
+
+@media screen and (min-width: 700px) and (max-width: 1300px){
+
+    #carouselExampleCaptions{
+
+        min-width: 600px;
+
+    }
+
+    .div-img{
+        width: 300px;
+    }
+
+}
+
+
+@media screen and (min-width: 420px) and (max-width: 600px){
+
+    #carouselExampleCaptions{
+
+        max-width: 350px !important;
+
+    }
+
+    .div-img{
+        width: 300px;
+    }
+
+}
+
+
 </style>
